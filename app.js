8a9b63bd267aa1bbef7a421f4b8cb53f4b7d0696
rastringer <ra1stringer@gmail.com>
2015-10-05 12:14:14 -0700

    initial

---
+++ app.js
@@ -0,0 +1,42 @@
+var pikePlace = {
+  location: 'Pike Place Market',
+  minCustHour: 17 ,
+  maxCustHour: 88,
+  avgCookiesCust: 5.2,
+  //calculates random num of cust/hr based on min/max
+  randCustHour: function() {
+return Math.floor(Math.random() * (maxCustHour - minCustHour + 1)) + minCustHour;
+totalCookieHour:
+makeUL = function....
+}
+}
+
+
+min
+max
+random
+
+random * cookies per customer = cookies per hour
+
+
+-------
+list:
+function makeUL(totalCookieHour){
+  for (var i = 0; i < array.length; i++){
+    // connect list to JS file
+   var awesomeList = document.getElementById('list1');
+// create list item:
+var item = document.getElementById('li');
+// set contents
+item.appendChild(document.createTextNode(array[i]));
+// add to list:
+awesomeList.appendChild(item);
+// return constructed list:
+return awesomeList;
+}
+var stuff = ['this', 'that', "another thing"];
+makeUL(stuff);
+
+
+  }
+}
