085ad080b797709b5dddbec07b694561baebe71e
rastringer <ra1stringer@gmail.com>
2015-10-06 14:13:42 -0700

    Wraps final functions into object constructor

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-var CookieStand = function(place, minCustHour, maxCustHour, avgCookiesCust) {
+var CookieStand = function(place, ulName, minCustHour, maxCustHour, avgCookiesCust) {
   this.place = place;
+  this.ulName = ulName;
   this.minCustHour = minCustHour;
   this.maxCustHour = maxCustHour;
   this.avgCookiesCust = avgCookiesCust;
@@ -16,80 +17,102 @@ var CookieStand = function(place, minCustHour, maxCustHour, avgCookiesCust) {
       this.dailyList.push(this.totalCookiesHour());
     };
   }
-}
-
-var pikePlace = new CookieStand('Pike Place', 17, 88, 5.2);
-var seaTac = new CookieStand('Sea Tac', 6, 44, 1.2);
-var southCenter = new CookieStand('South Center', 11, 38, 1.9);
-var bellevueSq = new CookieStand('Bellevue Square', 20, 48, 3.3);
-var alki = new CookieStand('Alki', 3, 24, 2.6);
-
-
-  makeUL = function(array) {
+   this.makeUL = function(array) {
     for (var i = 0; i < array.length; i++) {
-       var listPikePlace = document.getElementById('list1');
+       var ulElement = document.getElementById(ulName);
        var item = document.createElement('li');
        item.appendChild(document.createTextNode(this.hours[i] + ":00: " + array[i] + " cookies"));
-       listPikePlace.appendChild(item);
+       ulElement.appendChild(item);
       }
   }
+}
+var pikePlace = new CookieStand('Pike Place', 'pike', 17, 88, 5.2);
+var seaTac = new CookieStand('Sea Tac', 'seatac', 6, 44, 1.2);
+var southCenter = new CookieStand('South Center', 'Southcenter Mall', 11, 38, 1.9);
+var bellevueSq = new CookieStand('Bellevue Square', 'Bellevue Square', 20, 48, 3.3);
+var alki = new CookieStand('Alki', 'Alki', 3, 24, 2.6);
+
+
+//   makeUL = function(array) {
+//     for (var i = 0; i < array.length; i++) {
+//        var listPikePlace = document.getElementById('list1');
+//        var item = document.createElement('li');
+//        item.appendChild(document.createTextNode(this.hours[i] + ":00: " + array[i] + " cookies"));
+//        listPikePlace.appendChild(item);
+//       }
+//   }
 
 console.log(pikePlace);
 pikePlace.buildList();
-pikePlace.makeUL(pikePlace.dailyList)
-
-
-
-  makeUL = function(array) {
-    for (var i = 0; i < array.length; i++) {
-       var listseaTac = document.getElementById('list2');
-       var item = document.createElement('li');
-       item.appendChild(document.createTextNode(this.hours[i] + ":00: " + array[i] + " cookies"));
-       listseaTac.appendChild(item);
-     }
-  }
+pikePlace.makeUL(pikePlace.dailyList);
 
 console.log(seaTac);
 seaTac.buildList();
-seaTac.makeUL(seaTac.dailyList)
-
-  makeUL = function(array) {
-    for (var i = 0; i < array.length; i++) {
-       var listSouthCenter = document.getElementById('list3');
-       var item = document.createElement('li');
-       item.appendChild(document.createTextNode(this.hours[i] + ":00: " + array[i] + " cookies"));
-       listSouthCenter.appendChild(item);
-      }
-  }
+seaTac.makeUL(seaTac.dailyList);
 
 console.log(southCenter);
 southCenter.buildList();
-southCenter.makeUL(southCenter.dailyList)
+southCenter.makeUL(southCenter.dailyList);
 
-
-  makeUL = function(array) {
-    for (var i = 0; i < array.length; i++) {
-       var listBellevueSq = document.getElementById('list4');
-       var item = document.createElement('li');
-       item.appendChild(document.createTextNode(this.hours[i] + ":00: " + array[i] + " cookies"));
-       listBellevueSq.appendChild(item);
-      }
-  }
-
-console.log(bellevueSq);
+console.log(pikePlace);
 bellevueSq.buildList();
-bellevueSq.makeUL(bellevueSq.dailyList)
-
-
-  makeUL = function(array) {
-    for (var i = 0; i < array.length; i++) {
-       var listAlki = document.getElementById('list5');
-       var item = document.createElement('li');
-       item.appendChild(document.createTextNode(this.hours[i] + ":00: " + array[i] + " cookies"));
-       listAlki.appendChild(item);
-      }
-  }
+bellevueSq.makeUL(bellevueSq.dailyList);
 
 console.log(alki);
 alki.buildList();
-alki.makeUL(alki.dailyList)
+alki.makeUL(alki.dailyList);
+
+
+//   makeUL = function(array) {
+//     for (var i = 0; i < array.length; i++) {
+//        var listseaTac = document.getElementById('list2');
+//        var item = document.createElement('li');
+//        item.appendChild(document.createTextNode(this.hours[i] + ":00: " + array[i] + " cookies"));
+//        listseaTac.appendChild(item);
+//      }
+//   }
+
+// console.log(seaTac);
+// seaTac.buildList();
+// seaTac.makeUL(seaTac.dailyList)
+
+//   makeUL = function(array) {
+//     for (var i = 0; i < array.length; i++) {
+//        var listSouthCenter = document.getElementById('list3');
+//        var item = document.createElement('li');
+//        item.appendChild(document.createTextNode(this.hours[i] + ":00: " + array[i] + " cookies"));
+//        listSouthCenter.appendChild(item);
+//       }
+//   }
+
+// console.log(southCenter);
+// southCenter.buildList();
+// southCenter.makeUL(southCenter.dailyList)
+
+
+//   makeUL = function(array) {
+//     for (var i = 0; i < array.length; i++) {
+//        var listBellevueSq = document.getElementById('list4');
+//        var item = document.createElement('li');
+//        item.appendChild(document.createTextNode(this.hours[i] + ":00: " + array[i] + " cookies"));
+//        listBellevueSq.appendChild(item);
+//       }
+//   }
+
+// console.log(bellevueSq);
+// bellevueSq.buildList();
+// bellevueSq.makeUL(bellevueSq.dailyList)
+
+
+//   makeUL = function(array) {
+//     for (var i = 0; i < array.length; i++) {
+//        var listAlki = document.getElementById('list5');
+//        var item = document.createElement('li');
+//        item.appendChild(document.createTextNode(this.hours[i] + ":00: " + array[i] + " cookies"));
+//        listAlki.appendChild(item);
+//       }
+//   }
+
+// console.log(alki);
+// alki.buildList();
+// alki.makeUL(alki.dailyList)
