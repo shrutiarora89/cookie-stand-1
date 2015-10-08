ccfbbfb66dee8dda425d821053a5ab5ace6bb6b8
rastringer <ra1stringer@gmail.com>
2015-10-07 17:37:36 -0700

    Improves JS functions, adds sales, public html file, CSS

--- app.js
+++ app.js
@@ -4,62 +4,49 @@ var CookieStand = function(place, ulName, minCustHour, maxCustHour, avgCookiesCu
   this.minCustHour = minCustHour;
   this.maxCustHour = maxCustHour;
   this.avgCookiesCust = avgCookiesCust;
-  this.dailyList = dailyList = [];
-  this.hours = hours = [10, 11, 12, 13, 14, 15, 16, 17];
+  this.dailyList = [];
+  this.hours = [10, 11, 12, 13, 14, 15, 16, 17];
   this.randCustHour = function() {
   return Math.floor(Math.random() * (this.maxCustHour - this.minCustHour + 1))
     + this.minCustHour;
   };
+
   this.totalCookiesHour = function() {
     return Math.floor(this.randCustHour() * this.avgCookiesCust);
   };
+
   this.buildList = function(){
     for(var i = 0; i < 8; i++) {
       this.dailyList.push(this.totalCookiesHour());
-    };
-  }
-  // this.totalCookiesDay = function(){
-  //   this.dailyList = 0;
-  //     for(var i = 0; i < this.hours; i ++) {
-  //         this.dailyList.push(this.totalCookiesHour());
-  //         this.dailyList = this.dailyList + this.totalCookiesHour[i];
-  //    };
-  // }
-  this.makeUL = function(array) {
-    for (var i = 0; i < array.length; i++) {
-       var ulElement = document.getElementById(ulName);
+    }
+  };
+
+  this.totalCookiesDay = function(){
+    var sum = 0;
+      for(var i = 0; i < this.dailyList.length; i ++) {
+          sum = sum + this.dailyList[i];
+      }
+      return sum
+  };
+
+  this.makeUL = function() {
+    this.buildList();
+    for (var i = 0; i < this.dailyList.length; i++) {
+       var ulElement = document.getElementById(this.ulName);
        var item = document.createElement('li');
        item.appendChild(document.createTextNode(this.hours[i] + ":00: "
-        + array[i] + " cookies"));
+        + this.dailyList[i] + " cookies"));
        ulElement.appendChild(item);
-      };
-  }
+      }
+      var totalItems = document.createElement("li");
+      totalItems.appendChild(document.createTextNode("Total: " + this.totalCookiesDay()));
+      ulElement.appendChild(totalItems);
+  };
+
+  this.makeUL();
 }
 var pikePlace = new CookieStand('Pike Place', 'pike', 17, 88, 5.2);
 var seaTac = new CookieStand('Sea Tac', 'seatac', 6, 44, 1.2);
 var southCenter = new CookieStand('South Center', 'southcenter', 11, 38, 1.9);
 var bellevueSq = new CookieStand('Bellevue Square', 'bellevue', 20, 48, 3.3);
 var alki = new CookieStand('Alki', 'Alki', 3, 24, 2.6);
-
-console.log(pikePlace);
-// pikePlace.totalCookiesDay();
-pikePlace.buildList();
-pikePlace.makeUL(pikePlace.dailyList);
-
-console.log(seaTac);
-seaTac.buildList();
-seaTac.makeUL(seaTac.dailyList);
-
-console.log(southCenter);
-southCenter.buildList();
-southCenter.makeUL(southCenter.dailyList);
-
-console.log(pikePlace);
-bellevueSq.buildList();
-bellevueSq.makeUL(bellevueSq.dailyList);
-
-console.log(alki);
-alki.buildList();
-alki.makeUL(alki.dailyList);
-
-
